dfe634ea8e3d2d4c709dff2ee4034fe72736b1f0
ADRIANE VICTORIA CARDOSO CZYCZA <adriane_czycza@estudante.sesisenai.org.br>
2026-08-17 11:01:22 -0300

    Atividade de boletim em python

---
+++ Exercicio4.py
@@ -0,0 +1,8 @@
+boletim = {"adrizinha diva" :10,
+           "bruno labuboico" :10,
+           "bruno lombriscomico" :10,
+           "emanuel apelador" :10,
+           "guilherme betinha" :10}
+media = sum(boletim.values()) / 5
+
+print ("A media é:", media)
